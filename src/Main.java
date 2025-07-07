db52394f530880fe2c6eb78a1361a8c2d071585e
thisouza01 <tihsouza01@gmail.com>
2025-07-07 20:20:22 -0300

    Initial commit.

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+
+
+}
+
+/* criar classe
+    - BankAccount
+         - tem limite de cheque especial
+         - o valor do cheque especial é somado ao saldo da conta
+         - varia conforme a quantidade de dinheiro depsita na criação da conta
+         - Se o valor depositado na hora for <= 500 -> cheque especial de 50.00
+         - Se o valor depositado for > 500 -> cheque especial é 50% do valor depositado
+         * -  Caso utilize o cheuqe especial -> cobrar uma taxa de 20% do valor usado no cheque
+
+ */
+
+// consultar o saldo
+
+/* selecionado a opção para consultar saldo
+    verificar se tem saldo
+        caso sim
+            retorna o saldo
+        caso nao
+            retorna 0
+
+ */
+// consultar cheque especial
+
+// depositar dinheiro
+
+// sacar dinheiro
+
+// pagar um boleto
+
+// verificar se a conta esta utillizando cheque especial
+
+

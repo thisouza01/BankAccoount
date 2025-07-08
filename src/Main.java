b81eecbdbec8afeaf3278c4de5c81bb21f880913
thisouza01 <tihsouza01@gmail.com>
2025-07-08 20:49:36 -0300

    Loop para escolha.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         var scanner = new Scanner(System.in);
 
@@ -12,52 +12,57 @@ public class Main {
         System.out.println("Valor do depósito inicial: ");
         double balance = scanner.nextDouble();
 
-        System.out.println("1- Consultar \n2- Sacar \n3- Depositar");
-        int option = scanner.nextInt();
-
         double limitOverdraft = (balance > 500 ? balance * 0.50 : 50);
 
         var bank1 = new BankAccount(name, balance, limitOverdraft);
 
-        switch (option){
-            case 1:
-                System.out.println("Saldo atual: " + bank1.getBalance() + " | Cheque especial: " + bank1.getOverdraft());
-                break;
-            case 2:
-                System.out.println("Qual o valor do saque: ");
-                double amountWithdraw = scanner.nextDouble();
+        int option = 0;
 
-                if (bank1.withdraw(amountWithdraw)){
-                    System.out.println("Saque efetuado! Saldo atual: " + bank1.getBalance());
-                } else {
-                    System.out.println("Saque não efetuado! Saldo atual: " + bank1.getBalance());
-                }
+        while(option != 4) {
+            System.out.println("1- Consultar \n2- Sacar \n3- Depositar\n4- Sair");
+            option = scanner.nextInt();
 
-                if (bank1.isUsingOverdraf()) { System.out.println("Utilizou o cheque especial! Saldo atual dele: " + bank1.getOverdraft()); }
+            switch (option) {
+                case 1:
+                    System.out.println("Saldo atual: " + (bank1.getBalance()+bank1.getOverdraft()) + " | Cheque especial: " + bank1.getOverdraft());
+                    break;
 
-                break;
-            case 3:
-                System.out.println("Qual o valor do depósito: ");
-                double amountDeposit = scanner.nextDouble();
-                if (bank1.deposit(amountDeposit)) {
-                    System.out.println("Deposito efetuado! Saldo atual: " + bank1.getBalance());
-                }
-                break;
+                case 2:
+                    System.out.println("Qual o valor do saque: ");
+                    double amountWithdraw = scanner.nextDouble();
+
+                    if (bank1.withdraw(amountWithdraw)) {
+                        System.out.println("Saque efetuado! Saldo atual: " + (bank1.getBalance()+bank1.getOverdraft()));
+                    } else {
+                        System.out.println("Saque não efetuado! Saldo atual: " + (bank1.getBalance()+bank1.getOverdraft()));
+                    }
+
+                    if (bank1.isUsingOverdraf()) {
+                        System.out.println("Utilizou o cheque especial! Saldo atual dele: " + bank1.getOverdraft());
+                    }
+
+                    break;
+
+                case 3:
+                    System.out.println("Qual o valor do depósito: ");
+                    double amountDeposit = scanner.nextDouble();
+                    if (bank1.deposit(amountDeposit)) {
+                        System.out.println("Deposito efetuado! Saldo atual: " + bank1.getBalance());
+                    }
+                    break;
+
+                default:
+
+            }
         }
 
 
-
-
-
-
-
-
-
+    }
 }
 
 
 
-}
+
 
 /* criar classe
     - BankAccount
